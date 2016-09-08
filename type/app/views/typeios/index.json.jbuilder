190a65d4d1b16e60e24b5349a68255d58d78fb0b
werein101 <ajaycuram.96@gmail.com>
2016-09-08 12:53:26 -0700

    first master

---
+++ type/app/views/typeios/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.array! @typeios, partial: 'typeios/typeio', as: :typeio
---
+++ type/test/controllers/typeios_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class TypeiosControllerTest < ActionController::TestCase
+  setup do
+    @typeio = typeios(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:typeios)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create typeio" do
+    assert_difference('Typeio.count') do
+      post :create, typeio: { body: @typeio.body, username: @typeio.username }
+    end
+
+    assert_redirected_to typeio_path(assigns(:typeio))
+  end
+
+  test "should show typeio" do
+    get :show, id: @typeio
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @typeio
+    assert_response :success
+  end
+
+  test "should update typeio" do
+    patch :update, id: @typeio, typeio: { body: @typeio.body, username: @typeio.username }
+    assert_redirected_to typeio_path(assigns(:typeio))
+  end
+
+  test "should destroy typeio" do
+    assert_difference('Typeio.count', -1) do
+      delete :destroy, id: @typeio
+    end
+
+    assert_redirected_to typeios_path
+  end
+end

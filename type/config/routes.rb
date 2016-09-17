7fd22c586ff9d92a7c5e4ea6e4afca7947e3fa65
Snip3r95 <gmanx101@gmail.com>
2016-09-16 19:20:09 -0700

    Added Comments
    Added a blank Comments controller

--- type/config/routes.rb
+++ type/config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :users do
     resources :typeios
   end
-  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

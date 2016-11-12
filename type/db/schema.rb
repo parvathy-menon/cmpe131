c254e96f4bbb753f131d58186f320ffe661374a0
Parvathy Menon <parvathy.menon1997@gmail.com>
2016-11-12 15:57:40 -0800

    migrations

--- type/db/schema.rb
+++ type/db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110181852) do
+ActiveRecord::Schema.define(version: 20161112233804) do
+
+  create_table "bulletin_boards", force: :cascade do |t|
+    t.integer  "num_posts"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "likes"
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"

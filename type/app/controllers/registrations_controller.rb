b23279785c778578bef9daabdd2bcc725878e4ed
Parvathy Menon <parvathy.menon1997@gmail.com>
2016-11-13 10:55:49 -0800

    Edited registration controller

--- type/app/controllers/registrations_controller.rb
+++ type/app/controllers/registrations_controller.rb
@@ -1,6 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
-
-  private
+    def create
+        super
+        b = BulletinBoard.create
+        resource.update bulletin_board: b
+    end
 
   def sign_up_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
